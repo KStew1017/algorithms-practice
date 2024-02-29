5edb939c4e9ef5187699e50f624e3238b3da4b06
Kyle Stewart <kstew1017@outlook.com>
2024-02-29 17:45:00 -0600

    CompareBinarytrees, DFSonBTS, MinHeap

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,17 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    const s: BinaryNode<number>[] = [head];
 
+    while (s.length) {
+        const curr = s.pop() as BinaryNode<number>;
+        if (curr.value === needle) {
+            return true;
+        }
+        if (curr.right) {
+            s.push(curr.right);
+        }
+        if (curr.left) {
+            s.push(curr.left);
+        }
+    }
+    return false;
 }
